5108f82e31d5ef3e7ffb47e9148c92e31f07a761
yanniromano <yanniromano@hotmail.com>
2022-03-18 16:15:05 -0300

    Decotouch Tienda

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>Tienda Deco Touch
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
