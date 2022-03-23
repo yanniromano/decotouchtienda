45a16ec1863acab476fe83790c70747e09082cd4
yanniromano <yanniromano@hotmail.com>
2022-03-22 22:09:29 -0300

    Decotouch Tienda - Clase 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Tienda Deco Touch
-        </h3>
+        <h3 style={{ color: 'white', margintop: '50px' }}> Próximamente Tienda Deco Touch</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
-    </div>
+    </div >
   );
 }
 
