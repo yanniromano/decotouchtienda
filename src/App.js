322da8c72718bb784d68f0feeac33caba910ab82
yanniromano <yanniromano@hotmail.com>
2022-03-31 22:31:44 -0300

    Desafío: Contador con botón

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 import Counter from './components/ItemCount/ItemCount';
+import './components/ItemCount/ItemCount.css';
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(true)
+
+  const handleOnAdd = (quantity) => {
+    console.log(`se agregaron ${quantity} productos`)
+  }
+
+
   return (
     <div className="App">
 
       <NavBar />
       <header className="App-header">
         <ItemListContainer greeting={'Hola Decotouch'}></ItemListContainer>
-        <Counter />
+        <button onClick={() => setShow(!show)} className="buttonCerrar">{show ? 'x' : '▼'}</button>
+        {show ? <Counter initial={0} stock={20} onAdd={handleOnAdd} /> : null}
       </header>
 
 
--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,16 +1,19 @@
-import './ItemCount.css';
+import './ItemCount.css'
 import { useState, useEffect } from 'react'
-
 
 const ItemCount = ({ initial, stock, onAdd }) => {
     const [count, setCount] = useState(0)
+
+
+    useEffect(() => {
+        console.log('cambio el count')
+    }, [count])
 
 
     const decrement = () => {
         if (count > 0) {
             setCount(count => count - 1);
         }
-
     }
 
     const increment = () => {
@@ -18,6 +21,7 @@ const ItemCount = ({ initial, stock, onAdd }) => {
             setCount(count => count + 1);
         }
     }
+    console.log('Esto esta en el cuerpo del componente')
 
 
 
@@ -26,8 +30,9 @@ const ItemCount = ({ initial, stock, onAdd }) => {
             <button onClick={decrement} className="buttonDecrement">-</button>
             <p>{count}</p>
             <button onClick={increment} className="buttonIncrement">+</button>
+            <button onClick={() => onAdd(count)} className="agregar"> Agregar al carrito</button>
         </div>
     )
 }
 
-export default ItemCount
+export default ItemCount 
